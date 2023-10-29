fb0f3bbd92081950bae6fa18ab31aac191bdc307
Lewis Simmonds <lslewissimmonds@googlemail.com>
2023-10-29 16:05:31 +0000

    Add X and O functionality

--- script.js
+++ script.js
@@ -3,16 +3,27 @@ let turnCount = 0;
 let gameNumber = 0;
 
 //function to run whenever box clicked
-const eachTurn = () => {
+const eachTurn = box_id => {
 
     let values = [];
     let boxes = [];
 
+    let player;
+    player = (turnCount % 2 === 0) ? "X" : "O";
+
     for (let i = 0; i <= 8; i++) {
-        values.push(document.getElementById(`${i}`).value);
+        values.push(document.getElementById(`${i}`).innerHTML);
         boxes.push(document.getElementById(`${i}`));
     };
 
-}
+    if (values[box_id] === "X" || values[box_id] === "O") {
+        return;
+    } else {
+        boxes[box_id].innerHTML = player;
+    };
 
-eachTurn();
+    console.log(values);
+    //increment turn count
+    turnCount++;
+
+};
